8fcf8121e2abbc95c5405bb061b842ba36e9f619
hayk003l <haykharutyunyan113@gmail.com>
2024-04-19 13:39:16 +0400

    main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class Car {
+    private:
+    std::string makeName = "Mercedes-Benz";
+    std::string modelname = "E-class";
+    int year = 2024;
+    std::string engineType = "Gasoline";
+    std::string color = "White";
+    std::string transmission = "Automatic";
+    int horsePower = 350;
+};
+
+
+int main() {
+
+    Car obj;
+    return 0;
+}
